8256fe7b6eac4bb6f2eaf954194840bbdac5a272
NureDeen <nur.e.deen.offical@gmail.com>
2023-12-19 20:59:14 +0600

    a

---
+++ Volley/src/main/java/com/volley/response/String.java
@@ -0,0 +1,31 @@
+package com.volley.response;
+
+import android.content.Context;
+import android.util.Log;
+import android.widget.Toast;
+
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
+
+public class String {
+    public static void RequestString (String url, Context context){
+        RequestQueue queue = Volley.newRequestQueue(context);
+        StringRequest stringRequest = new StringRequest(Request.Method.GET, url,
+                new Response.Listener<String>() {
+                    @Override
+                    public void onResponse(String response) {
+
+                    }
+                }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+            }
+        });
+
+        queue.add(stringRequest);
+    }
+}
